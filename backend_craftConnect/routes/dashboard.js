369b7e7cd708de0f001ad4d0c8cb48571b19f223
Rohit <rs3209438@gmail.com>
2024-07-25 02:28:01 +0530

    delete files

--- backend_craftConnect/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as dashboardController from "../controllers/dashboard.js";
-
-const router = Router();
-
-router.get("/data", dashboardController.getDashboardData);
-
-export default router;
